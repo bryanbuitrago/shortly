a2833fdce8859b869a18a473035bdb1643b6e799
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 18:11:06 -0400

    Add HeroSection component.

---
+++ src/components/hero/HeroSection.jsx
@@ -0,0 +1,22 @@
+import HeroContent from './HeroContent';
+import HeroImage from './HeroImage';
+
+const HeroSection = () => {
+  return (
+    <section id='hero'>
+      {/* Container */}
+      <div className='flex flex-col-reverse mx-auto p-6 lg:flex-row'>
+        {/* Left Side Content Container */}
+        <div className='flex flex-col space-y-10 mb-44 lg:mt-16 lg:w-1/2 lg:text-left'>
+          <HeroContent />
+        </div>
+        {/* Right Side Image Container */}
+        <div className='mb-24 mx-auto md:w-180 lg:mb-0 lg:w-1/2'>
+          <HeroImage />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
