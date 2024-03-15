ee5dc7bcd753559a5282b2286dff4233217715cb
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 18:28:26 -0400

    Add shortenData.js file with URLS_DATA constant.

---
+++ src/components/shorten/shortenData.js
@@ -0,0 +1,14 @@
+export const URLS_DATA = [
+  {
+    standardUrl: 'https://frontendmentor.io',
+    shortenUrl: 'https://rel.ink/k4IKyk',
+  },
+  {
+    standardUrl: 'https://twitter.com/frontendmentor',
+    shortenUrl: 'https://rel.ink/gxOXp9',
+  },
+  {
+    standardUrl: 'https://linkedin.com/frontend-mentor',
+    shortenUrl: 'https://rel.ink/gob3X9',
+  },
+];
