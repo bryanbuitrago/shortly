f43d1f883a01978588582b9cbb1c9c47a1256d53
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 17:09:37 -0400

    Update tailwind.config.js with new colors, fonts, spacing, and screen sizes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%) ',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        red: 'hsl(0, 87%, 67%)',
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
+      },
+      screens: {
+        sm: { min: '480px' },
+        md: { min: '768px' },
+        lg: { min: '976px' },
+        xl: { min: '1440px' },
       },
     },
   },
   plugins: [],
-}
+};
