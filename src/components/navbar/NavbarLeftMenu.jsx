54c22a538037885d84d706b24e1ff8707e21137e
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 17:39:09 -0400

    Add NavbarLeftMenu component.

---
+++ src/components/navbar/NavbarLeftMenu.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+const NavbarLeftMenu = () => {
+  return (
+    <div className='hidden space-x-8 font-bold md:flex'>
+      <Link
+        className='text-shortly-grayishViolet hover:text-shortly-veryDarkViolet'
+        href='#'
+      >
+        Features
+      </Link>
+      <Link
+        className='text-shortly-grayishViolet hover:text-shortly-veryDarkViolet'
+        href='#'
+      >
+        Pricing
+      </Link>
+      <Link
+        className='text-shortly-grayishViolet hover:text-shortly-veryDarkViolet'
+        href='#'
+      >
+        Resources
+      </Link>
+    </div>
+  );
+};
+
+export default NavbarLeftMenu;
