272e2e10352c6a25af685ae3f8e45c83799f4faf
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-15 17:39:21 -0400

    Add NavBar component.

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState } from 'react';
+import HamburgerButton from '../HamburgerButton';
+import MobileMenu from '../MobileMenu';
+import NavbarLeftMenu from './NavbarLeftMenu';
+import NavbarLogo from './NavbarLogo';
+import NavbarRightBtns from './NavbarRightBtns';
+
+const NavBar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const onClickToggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+  return (
+    <nav className='relative mx-auto p-6'>
+      {/* Container */}
+      <div className='flex items-center justify-between'>
+        {/* Logo/Menu Container */}
+        <div className='flex items-center space-x-20'>
+          <NavbarLogo />
+          <NavbarLeftMenu />
+        </div>
+        {/* Right Buttons Menu */}
+        <NavbarRightBtns />
+        {/* Hamburger Button */}
+        <HamburgerButton
+          onClickToggleMobileMenu={onClickToggleMobileMenu}
+          isMobileMenuOpen={isMobileMenuOpen}
+        />
+      </div>
+      {/* Mobile Menu */}
+      <MobileMenu
+        onClickToggleMobileMenu={onClickToggleMobileMenu}
+        isMobileMenuOpen={isMobileMenuOpen}
+      />
+    </nav>
+  );
+};
+
+export default NavBar;
