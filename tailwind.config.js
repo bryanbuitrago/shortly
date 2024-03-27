873e7f05693eda8e1f78ad99b6427d67acae7a5a
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-26 23:45:12 -0400

    Update color names.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        veryDarkBlue: 'hsl(255, 11%, 22%)',
-        darkViolet: 'hsl(257, 27%, 26%)',
-        veryDarkViolet: 'hsl(260, 8%, 14%) ',
-        grayishViolet: 'hsl(257, 7%, 63%)',
-        red: 'hsl(0, 87%, 67%)',
-        cyan: 'hsl(180, 66%, 49%)',
-        cyanLight: 'hsl(180, 66%, 69%)',
+        'shortly-cyan': 'hsl(180, 66%, 49%)',
+        'shortly-cyanLight': 'hsl(180, 66%, 69%)',
+        'shortly-darkViolet': 'hsl(257, 27%, 26%)',
+        'shortly-red': 'hsl(0, 87%, 67%)',
+        'shortly-grayishViolet': 'hsl(257, 7%, 63%)',
+        'shortly-veryDarkBlue': 'hsl(255, 11%, 22%)',
+        'shortly-veryDarkViolet': 'hsl(260, 8%, 14%) ',
+        //  ===================================
+        // veryDarkBlue: 'hsl(255, 11%, 22%)',
+        // darkViolet: 'hsl(257, 27%, 26%)',
+        // veryDarkViolet: 'hsl(260, 8%, 14%) ',
+        // grayishViolet: 'hsl(257, 7%, 63%)',
+        // red: 'hsl(0, 87%, 67%)',
+        // cyan: 'hsl(180, 66%, 49%)',
+        // cyanLight: 'hsl(180, 66%, 69%)',
       },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
