efd638bea4a671ecac6fb653bf3c62ac6ab2d186
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-26 23:45:31 -0400

    Add new sections.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 import HeroSection from '@/components/hero/HeroSection';
 import NavBar from '@/components/navbar/NavBar';
-
+import ShortenSection from '@/components/shorten/ShortenSection';
+import FeaturesSection from '@/components/features/FeaturesSection';
+import StatsSection from '@/components/StatsSection';
+import CtaSection from '@/components/CtaSection';
+import Footer from '@/components/footer/Footer';
 export default function Home() {
   return (
     <main>
       <NavBar />
       <HeroSection />
+      <ShortenSection />
+      <StatsSection />
+      <FeaturesSection />
+      <CtaSection />
+      <Footer />
     </main>
   );
 }
