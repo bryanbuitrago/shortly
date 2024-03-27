380b490c4c46e24a09ef402b7648c11293493ee4
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-26 23:48:42 -0400

    Add featuresData.js with initial data.

---
+++ src/components/features/featuresData.js
@@ -0,0 +1,20 @@
+export const BOXES_DATA = [
+  {
+    imgSrc: '/images/icon-brand-recognition.svg',
+    title: 'Brand Recognition',
+    description:
+      "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.",
+  },
+  {
+    imgSrc: '/images/icon-detailed-records.svg',
+    title: 'Detailed records',
+    description:
+      'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+  },
+  {
+    imgSrc: '/images/icon-fully-customizable.svg',
+    title: 'Fully customizable',
+    description:
+      'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+  },
+];
