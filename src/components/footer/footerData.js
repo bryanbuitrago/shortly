d1bd79bac54a783bdaa904fc5249e70cd60f8270
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-26 23:47:29 -0400

    Add footerData.js with column data.

---
+++ src/components/footer/footerData.js
@@ -0,0 +1,14 @@
+export const COLUMN_DATA = [
+  {
+    title: 'Features',
+    menuItems: ['Link shortening', 'Branded links', 'Analytics'],
+  },
+  {
+    title: 'Resources',
+    menuItems: ['Blog', 'Developers', 'Support'],
+  },
+  {
+    title: 'Company',
+    menuItems: ['About', 'Our Team', 'Careers', 'Contact'],
+  },
+];
