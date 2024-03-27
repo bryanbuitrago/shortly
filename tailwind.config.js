a5c922462af80a1454d5c873f268a3a00cf61af6
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-26 23:57:36 -0400

    Update color name.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       colors: {
         'shortly-cyan': 'hsl(180, 66%, 49%)',
-        'shortly-cyanLight': 'hsl(180, 66%, 69%)',
+        'cyan-light': 'hsl(180, 66%, 69%)',
         'shortly-darkViolet': 'hsl(257, 27%, 26%)',
         'shortly-red': 'hsl(0, 87%, 67%)',
         'shortly-grayishViolet': 'hsl(257, 7%, 63%)',
