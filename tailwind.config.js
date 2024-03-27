3d57464bda6f2868c2e4fba6d01bd97916b0eee4
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-27 00:11:35 -0400

    Remove unused color variables.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,14 +15,6 @@ module.exports = {
         grayishViolet: 'hsl(257, 7%, 63%)',
         veryDarkBlue: 'hsl(255, 11%, 22%)',
         veryDarkViolet: 'hsl(260, 8%, 14%)',
-        //  ===================================
-        // veryDarkBlue: 'hsl(255, 11%, 22%)',
-        // darkViolet: 'hsl(257, 27%, 26%)',
-        // veryDarkViolet: 'hsl(260, 8%, 14%) ',
-        // grayishViolet: 'hsl(257, 7%, 63%)',
-        // red: 'hsl(0, 87%, 67%)',
-        // cyan: 'hsl(180, 66%, 49%)',
-        // cyanLight: 'hsl(180, 66%, 69%)',
       },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
